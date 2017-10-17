37d12899f5fda8d48e42f6fe5b7f031aa87447d4
jmkiley <jordan.kiley@mapbox.com>
2017-10-16 17:37:31 -0700

    clustering plugin

---
+++ ClusteringPlugin/MBXClusteringPlugin.h
@@ -0,0 +1,13 @@
+//
+//  MBXClusteringPlugin.h
+//  TestApp
+//
+//  Created by Jordan Kiley on 10/4/17.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBXClusteringPlugin : NSObject
+
+@end
---
+++ ClusteringPlugin/MBXClusteringPlugin.m
@@ -0,0 +1,14 @@
+//
+//  MBXClusteringPlugin.m
+//  TestApp
+//
+//  Created by Jordan Kiley on 10/4/17.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#import "MBXClusteringPlugin.h"
+#import "MapboxPluginKit.h"
+
+@implementation MBXClusteringPlugin
+
+@end
