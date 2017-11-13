dda2bc03fffa2cf76561991b39f8c8c9060cfc21
jmkiley <jordan.kiley@mapbox.com>
2017-11-13 10:39:59 -0800

    MBXStaticCluster is a WIP

---
+++ ClusteringPlugin/Clustering/Algorithm/MBCClusterAlgorithm.h
@@ -0,0 +1,13 @@
+//
+//  MBCClusterAlgorithm.h
+//  MapboxPlugins
+//
+//  Created by Jordan Kiley on 11/10/17.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#ifndef MBCClusterAlgorithm_h
+#define MBCClusterAlgorithm_h
+
+
+#endif /* MBCClusterAlgorithm_h */
--- ClusteringPlugin/Clustering/MBXStaticCluster.m
+++ ClusteringPlugin/Clustering/MBXStaticCluster.m
@@ -8,6 +8,32 @@
 
 #import "MBXStaticCluster.h"
 
-@implementation MBXStaticCluster
+@implementation MBXStaticCluster {
+    NSMutableArray<id<MBXClusterItem>> *_clusterItems;
+}
 
+- (instancetype)initWithClusterPosition:(CLLocationCoordinate2D)clusterPosition {
+    if (self = [super init]) {
+        _clusterItems = [[NSMutableArray alloc] init];
+        _clusterPosition = clusterPosition;
+    }
+    return self;
+}
+
+- (NSUInteger)itemCount {
+    return _clusterItems.count;
+}
+
+- (NSArray<id<MBXClusterItem>> *)clusterItems {
+    return [_clusterItems copy];
+}
+
+// JK: Can I just call `addObject` instead?
+- (void)addItem:(id<MBXClusterItem>)clusterItem {
+    [_clusterItems addObject:clusterItem];
+}
+
+- (void)removeItem:(id<MBXClusterItem>)clusterItem {
+    [_clusterItems removeObject:clusterItem];
+}
 @end
